706f71366d864c01ed269290bc23aaf3c6e18f99
Dineshkumar Venkatesan <dn583714@dal.ca>
2023-10-18 12:26:02 -0300

    finished lab1

---
+++ lab1/task3.py
@@ -0,0 +1,11 @@
+import logging
+logger = logging.getLogger('example_logger')
+logger.warning('This is a warning')except Exception as e:
+logging.error("Exception occurred", exc_info=True)import logging
+a = 5
+b = 0
+try:
+c = a / b
+import logging
+name = 'John'
+error(f'{name} raised an error')
